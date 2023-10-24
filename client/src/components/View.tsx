0da16c2d0342d967f94d9b9ce401bbcc97998e9f
aayushmahapatra <mahapatraaayush@gmail.com>
2023-10-24 22:47:57 +0530

    Add support for web3storage

--- client/src/components/View.tsx
+++ client/src/components/View.tsx
@@ -2,16 +2,62 @@ import { FC } from "react";
 
 interface IView {
   loading: boolean;
-  protocolLinks: string[];
+  uploadArray: {
+    uploadId: string;
+    cid?: string;
+    protocolLink?: string;
+    status?: string;
+  }[];
   error?: boolean;
+  handleItemClick: (uploadId: string) => void;
+  bucketId: string;
+  setBucketId: (bucketId: string) => void;
+  handleFetchBucketUploads: (bucketId: string) => void;
+  accessToken: string;
+  setAccessToken: (accessToken: string) => void;
 }
 
-const View: FC<IView> = ({ loading, protocolLinks, error }) => {
+const View: FC<IView> = ({
+  loading,
+  uploadArray,
+  error,
+  handleItemClick,
+  bucketId,
+  setBucketId,
+  handleFetchBucketUploads,
+  accessToken,
+  setAccessToken,
+}) => {
   return (
     <div className="bg-white rounded p-6 shadow mt-4">
       <h2 className="bg-blue-100 py-3 px-6 mb-4 rounded text-gray-600 text-lg font-bold shadow">
         View Pinned CIDs
       </h2>
+      <div className="mb-8 flex justify-center gap-4">
+        <input
+          className="h-full w-full border border-gray-200 rounded py-2 px-3"
+          type="password"
+          placeholder="spheron access token"
+          value={accessToken}
+          onChange={(e) => setAccessToken(e.target.value)}
+        />
+
+        <input
+          className="h-full w-full border border-gray-200 rounded py-2 px-3"
+          type="text"
+          placeholder="add bucket id to fetch previous uploads"
+          value={bucketId}
+          onChange={(e) => setBucketId(e.target.value)}
+        />
+        <button
+          className={`bg-orange-400 py-1 px-10 rounded text-white font-semibold flex gap-2 items-center ${
+            bucketId ? "cursor-pointer" : "cursor-not-allowed"
+          }`}
+          onClick={() => handleFetchBucketUploads(bucketId)}
+        >
+          Fetch
+        </button>
+      </div>
       {loading && (
         <div className="h-[40vh] flex flex-col items-center justify-center">
           <div
@@ -20,7 +66,7 @@ const View: FC<IView> = ({ loading, protocolLinks, error }) => {
           />
 
           <h3 className="text-gray-600 font-semibold text-center">
-            Pinning CIDs...
+            Loading...
           </h3>
         </div>
       )}
@@ -32,14 +78,34 @@ const View: FC<IView> = ({ loading, protocolLinks, error }) => {
         </div>
       )}
       <div>
-        {protocolLinks.map((link, i) => (
-          <div
-            key={i}
-            className="bg-gray-50 py-1 px-2 my-4 rounded text-gray-600 text-xs font-light shadow cursor-pointer hover:text-blue-600"
-          >
-            <a href={link}>
-              {i + 1}. {link}
-            </a>
+        {uploadArray.map((uploadItem, i) => (
+          <div>
+            {uploadItem?.protocolLink ? (
+              <a href={uploadItem.protocolLink}>
+                <div
+                  key={i}
+                  className="bg-gray-50 py-1 px-2 my-4 rounded text-gray-600 text-xs font-light shadow cursor-pointer hover:text-blue-600"
+                >
+                  {i + 1}. {uploadItem.protocolLink} - {uploadItem.status}
+                </div>
+              </a>
+            ) : uploadItem?.status ? (
+              <div
+                key={i}
+                className="bg-gray-50 py-1 px-2 my-4 rounded text-gray-600 text-xs font-light shadow cursor-pointer hover:text-blue-600"
+                onClick={() => handleItemClick(uploadItem.uploadId)}
+              >
+                {i + 1}. {uploadItem.uploadId} - {uploadItem.status}
+              </div>
+            ) : (
+              <div
+                key={i}
+                className="bg-gray-50 py-1 px-2 my-4 rounded text-gray-600 text-xs font-light shadow cursor-pointer hover:text-blue-600"
+                onClick={() => handleItemClick(uploadItem.uploadId)}
+              >
+                {i + 1}. {uploadItem.uploadId} - Pinning
+              </div>
+            )}
           </div>
         ))}
       </div>
